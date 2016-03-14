49288bf7e3cf103e87fa32a53234c35b403b9f90
hujiaju <nanhujiaju@gmail.com>
2016-03-14 17:17:40 +0800

    Initial Commit

---
+++ NHKeyboardPro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHKeyboardPro
+//
+//  Created by hu jiaju on 16/3/14.
+//  Copyright © 2016年 hu jiaju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHKeyboardPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NHKeyboardPro
+//
+//  Created by hu jiaju on 16/3/14.
+//  Copyright © 2016年 hu jiaju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
